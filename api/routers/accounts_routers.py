c881abb6310557076efc0bfe86d70aab57899591
brittanyrobarge26 <brittanyrobarge26@gmail.com>
2024-03-05 12:57:57 -0500

    beverage auth added

--- api/routers/accounts_routers.py
+++ api/routers/accounts_routers.py
@@ -45,3 +45,15 @@ async def get(
             "type": "Bearer",
             "account": account,
         }
+
+@router.get("/token", response_model=AccountToken | None)
+async def get_token(
+    request: Request,
+    account: Account = Depends(authenticator.try_get_current_account_data)
+) -> AccountToken | None:
+    if account and authenticator.cookie_name in request.cookies:
+        return {
+            "access_token": request.cookies[authenticator.cookie_name],
+            "type": "Bearer",
+            "account": account,
+        }  
