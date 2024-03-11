c8358ce627613b5fcd0d3dbba58786ce13446fc0
brittanyrobarge26 <brittanyrobarge26@gmail.com>
2024-03-11 14:51:56 -0400

    adding updated backend to frontend started

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,9 +1,11 @@
 // This makes VSCode check types as if you are using TypeScript
 //@ts-check
 import { useState, useEffect } from 'react'
-import ErrorNotification from './ErrorNotification'
+import Error from './pages/Error'
 import Construct from './Construct'
 import './App.css'
+import Nav from './components/Nav'
+import { Outlet } from 'react-router-dom'
 
 // All your environment variables in vite are in this object
 console.table(import.meta.env)
@@ -11,10 +13,6 @@ console.table(import.meta.env)
 // When using environment variables, you should do a check to see if
 // they are defined or not and throw an appropriate error message
 const API_HOST = import.meta.env.VITE_API_HOST
-
-if (!API_HOST) {
-    throw new Error('VITE_API_HOST is not defined')
-}
 
 /**
  * This is an example of using JSDOC to define types for your component
